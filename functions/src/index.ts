9cd921c5d20b3f2ad47444e7ef0e8a89b6fcdc94
LuanBonetto <luanbonetto42@gmail.com>
2020-11-22 02:06:14 -0300

    configure firebase and express

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,53 @@
-import * as functions from 'firebase-functions';
+import * as functions from "firebase-functions";
+import { AddressInfo } from "net";
+import * as express from "express";
+import * as cors from "cors";
+import * as admin from "firebase-admin";
+import firebase from "firebase";
+import { Request, Response } from 'express';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+const app = express();
+app.use(cors({ origin: true }), express.json());
+exports.app = functions.https.onRequest(app);
+
+const firebaseConfig = {
+  apiKey: functions.config().someservice.key,
+  authDomain: functions.config().someservice.auth,
+  databaseURL: functions.config().someservice.database,
+  projectId: functions.config().someservice.project,
+  storageBucket: functions.config().someservice.storage,
+  messagingSenderId: functions.config().someservice.messaging,
+  appId: functions.config().someservice.app,
+}
+
+admin.initializeApp(functions.config().firebase);
+firebase.initializeApp(firebaseConfig);
+
+const db = admin.firestore();
+
+app.get('/teste', async(req: Request, res: Response) => {
+  try {
+
+    // await db.collection('teste').doc().set({
+    //   nome: 'Luan',
+    //   sobrenome: 'Bonetto'
+    // })
+
+    res.status(200).send({
+      message: 'Hello World'
+    })
+  } catch (err) {
+    res.status(400).send({
+      message: err.message
+    })
+  }
+})
+
+const server = app.listen(process.env.PORT || 3000, () => {
+  if (server) {
+    const address = server.address() as AddressInfo;
+    console.log(`Server is running in http://localhost:${address.port}`);
+  } else {
+    console.error(`Failure upon starting server.`);
+  }
 });
