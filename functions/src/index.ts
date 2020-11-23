3f3a95eb200477542d0a7192be6be11c4d0afaf5
LuanBonetto <luanbonetto42@gmail.com>
2020-11-22 21:31:58 -0300

    cofigure emulator

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,29 +1,31 @@
 import * as functions from "firebase-functions";
 import { AddressInfo } from "net";
-import * as cors from "cors";
+import cors from "cors";
 import * as admin from "firebase-admin";
-import firebase from "firebase";
-import * as express from 'express';
+// import firebase from "firebase";
+import express from 'express';
 import { endpointTeste } from './temporaryEndpoint';
 
 const app = express();
 app.use(cors({ origin: true }), express.json());
 exports.app = functions.https.onRequest(app);
 
-const firebaseConfig = {
-  apiKey: functions.config().someservice.key,
-  authDomain: functions.config().someservice.auth,
-  databaseURL: functions.config().someservice.database,
-  projectId: functions.config().someservice.project,
-  storageBucket: functions.config().someservice.storage,
-  messagingSenderId: functions.config().someservice.messaging,
-  appId: functions.config().someservice.app,
-}
+// const firebaseConfig = {
+//   apiKey: functions.config().someservice.key,
+//   authDomain: functions.config().someservice.auth,
+//   databaseURL: functions.config().someservice.database,
+//   projectId: functions.config().someservice.project,
+//   storageBucket: functions.config().someservice.storage,
+//   messagingSenderId: functions.config().someservice.messaging,
+//   appId: functions.config().someservice.app,
+// }
 
 admin.initializeApp(functions.config().firebase);
-firebase.initializeApp(firebaseConfig);
+// firebase.initializeApp(firebaseConfig);
 
 // const db = admin.firestore();
+
+export const db = admin.firestore();
 
 app.get('/teste', endpointTeste);
 
--- functions/src/temporaryEndpoint.ts
+++ functions/src/temporaryEndpoint.ts
@@ -1,12 +1,14 @@
 import { Request, Response } from 'express';
+import { db } from './index';
+
 
 export const endpointTeste = async(req: Request, res: Response) => {
     try {
   
-      // await db.collection('teste').doc().set({
-      //   nome: 'Luan',
-      //   sobrenome: 'Bonetto'
-      // })
+      await db.collection('teste').doc().set({
+        nome: 'Luan',
+        sobrenome: 'Bonetto'
+      })
   
       res.status(200).send({
         message: 'Hello World',
